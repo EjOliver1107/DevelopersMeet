3d33f70305887b9ab271679a207eaa4718b70583
hernandez-g <g5hernandez@yahoo.com>
2022-05-16 11:50:36 -0700

    urls change

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('users/', views.users_index, name='index'),
     path('accounts/signup/', views.signup, name="signup"),
-    path('users/<int:user_id>/add_photo/', views.add_photo, name='add_photo'),
+    path('profiles/<int:profile_id>/add_photo/', views.add_photo, name='add_photo'),
     # path('users/<int:user_id>/', views.users_detail, name='detail')
 
 ]

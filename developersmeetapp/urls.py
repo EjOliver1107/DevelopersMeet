5cbb57c223e9b41219b737d597b18109f9176069
smansfield5 <smansfield5@yahoo.com>
2022-05-16 16:12:06 -0600

    fixed database

--- developersmeetapp/urls.py
+++ developersmeetapp/urls.py
@@ -1,4 +1,4 @@
-"""DevelopersMeetApp URL Configuration
+"""developersmeetapp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/

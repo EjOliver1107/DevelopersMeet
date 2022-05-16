542ed50d26ee45570bf90e2bfd7bb80f7356d508
smansfield5 <smansfield5@yahoo.com>
2022-05-16 15:02:09 -0600

    profile

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from . import views 
+from . import views
+#path > defines each route
 
+#area where we define urls
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('accounts/signup/', views.signup, name='signup'),
+    path('profile/', views.profile_index, name='index'),
+    path('accounts/signup/', views.signup, name="signup"),
+    path('users/<int:user_id>/add_photo/', views.add_photo, name='add_photo'),
+    # path('users/<int:user_id>/', views.users_detail, name='detail')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,32 +1,59 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+import os
+import uuid
+import boto3
+from .models import Profile, Photo
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+@login_required
+def profile_index(request):
+    profile = Profile.objects.all()
+    return render(request, 'profile/index.html', { 'profile': profile })
+
+@login_required
+def profile_detail(request, profile_id):
+    profile = Profile.objects.get(id=profile_id)
+    return render(request, 'profile/detail.html', { 'profile': profile })
+    
+
+@login_required
+def add_photo(request, profile_id):
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        try:
+            bucket = os.environ['S3_BUCKET']
+            s3.upload_fileobj(photo_file, bucket, key)
+            url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
+            Photo.objects.create(url=url, profile_id=profile_id)
+        except Exception as e:
+            print('An error occurred uploading file to S3')
+            print(e)
+    return redirect('/profile/', profile_id=profile_id)
 
 def signup(request):
   error_message = ''
   if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
     form = UserCreationForm(request.POST)
     if form.is_valid():
-      # This will add the user to the database
-      user = form.save()
-      # This is how we log a user in via code
-      login(request, user)
+      profile = form.save()
+      login(request, profile)
       return redirect('about')
     else:
-      error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
+      error_message = 'Invalid sign up - Try Again'
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
-
-def home(request):
-    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
-
-def about(request):
-    return render(request, 'about.html')
 
 

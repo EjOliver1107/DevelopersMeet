89b350f7ca82d95808fb99731de2f8e1d03f9ea7
hernandez-g <g5hernandez@yahoo.com>
2022-05-16 10:37:23 -0700

    random edit

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from . import views 
+from . import views
+#path > defines each route
 
+#area where we define urls
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('accounts/signup/', views.signup, name='signup'),
+    path('users/', views.users_index, name='index'),
+    path('accounts/signup/', views.signup, name="signup"),
+    path('users/<int:user_id>/add_photo/', views.add_photo, name='add_photo'),
+    # path('users/<int:user_id>/', views.users_detail, name='detail')
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,32 +1,58 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+import os
+import uuid
+import boto3
+from .models import User, Photo
 
-def signup(request):
-  error_message = ''
-  if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      # This will add the user to the database
-      user = form.save()
-      # This is how we log a user in via code
-      login(request, user)
-      return redirect('about')
-    else:
-      error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
-  form = UserCreationForm()
-  context = {'form': form, 'error_message': error_message}
-  return render(request, 'registration/signup.html', context)
+# Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
+@login_required
+def users_index(request):
+    users = User.objects.all()
+    return render(request, 'users/index.html', { 'users': users })
+
+@login_required
+def users_detail(request, user_id):
+    user = User.objects.get(id=user_id)
+    return render(request, 'users/detail.html', { 'user': user })
+    
+
+@login_required
+def add_photo(request, user_id):
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        try:
+            bucket = os.environ['S3_BUCKET']
+            s3.upload_fileobj(photo_file, bucket, key)
+            url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
+            Photo.objects.create(url=url, user_id=user_id)
+        except Exception as e:
+            print('An error occurred uploading file to S3')
+            print(e)
+    return redirect('/users/', user_id=user_id)
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('about')
+    else:
+      error_message = 'Invalid sign up - Try Again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
 
